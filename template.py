8aede255c4904ce64a7c2af7431c6252cc17640e
Alexander <alexander.luyten@stud.unibas.ch>
2020-12-22 00:22:04 +0100

    updated template

--- template.py
+++ template.py
@@ -1,11 +1,20 @@
-def main(input):
+def part_1(input):
+    ...
+
+
+def part_2(input):
     ...
 
 
 if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+        ingoing = f.read()
+        lines = ingoing.split('\n')
+        print(f'Solution part 1: {part_1(lines)}')
 
-    ingoing = ''''''
-
-    lines = ingoing.split('\n')
+    with open('input.txt', 'r') as f:
+        ingoing = f.read()
+        lines = ingoing.split('\n')
+        print(f'Solution part 2: {part_2(lines)}')
 
     print(main(lines))
